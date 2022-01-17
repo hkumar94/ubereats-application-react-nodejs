188df9a569ab21c5443de37a67f386694e467d30
hkumar94 <97869121+hkumar94@users.noreply.github.com>
2022-01-16 23:17:12 -0800

    Update configValue.js

--- backend/config/configValue.js
+++ backend/config/configValue.js
@@ -1,10 +1,10 @@
 const config = {
     secret: "cmpe273_secret_key",
-    frontendURL: "http://18.117.253.179:3000",
-    mongoDB: "mongodb+srv://mangodbadmin:test%40123@cluster0.1mfun.mongodb.net/uber_eats?retryWrites=true&w=majority",
-    s3AccessKey : 'AKIARBESX7V42TFDAD45',
-    s3SecretAccessKey : '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',
-    s3region : 'us-east-2',
+    frontendURL: "",
+    mongoDB: "",
+    s3AccessKey : '',
+    s3SecretAccessKey : '',
+    s3region : '',
     s3BucketName : 'ubereatsimage'   
 };
 module.exports = config;
