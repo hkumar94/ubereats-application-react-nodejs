5e1aad651bdfa7ccae767686e0fecd49c3560e48
hkumar94 <97869121+hkumar94@users.noreply.github.com>
2022-01-16 22:46:01 -0800

    Update endPointUrl.js

--- frontend/src/endPointUrl.js
+++ frontend/src/endPointUrl.js
@@ -1,6 +1,6 @@
 const endPointObj = {
     url: 'http://localhost:3001',
-    //frontendServer: ' https://s3-us-east-2.amazonaws.com/ubereatsimage'
+    frontendServer: ' '
   };
 export default endPointObj;
   
