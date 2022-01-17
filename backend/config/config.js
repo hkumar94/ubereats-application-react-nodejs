0e054ff0111bd9480c64076107ba37c2a8548cb6
hkumar94 <97869121+hkumar94@users.noreply.github.com>
2022-01-16 23:15:57 -0800

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,4 +1,4 @@
 const config = {
-    mongoDB: "mongodb+srv://mangodbadmin:test%40123@cluster0.1mfun.mongodb.net/uber_eats?retryWrites=true&w=majority"   
+    mongoDB: ""   
 };
 module.exports = config;
